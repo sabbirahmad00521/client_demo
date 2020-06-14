6bcad429e60103e05443c88c4ff20f6a32b46f65
sabbirahmad00521 <sabbirahmad00521@gmail.com>
2020-06-14 17:44:42 +0600

    new

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,4 +4,26 @@ jQuery(document).ready(function ($) {
       jQuery(this).css("color", "black")
    })
 
-}); 
+   // menu
+   jQuery("a.responsive-menu").click(function () {
+      jQuery("ul.menu").fadeToggle();
+      return false;
+   });
+
+   jQuery(window).resize(function () {
+      var windowWidth = jQuery(window).width();
+      if (windowWidth > 991) {
+         jQuery("ul.menu").css({
+            "display": "block"
+         });
+         $('.responsive-menu').removeClass('change');
+      } else {
+         jQuery("ul.menu").css({
+            "display": "none"
+         });
+
+      }
+   });
+
+
+});
