ece720b66458adf983d515154f33852ad8878cb2
sabbirahmad00521 <sabbirahmad00521@gmail.com>
2020-06-29 19:43:29 +0600

    as

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,44 +1,59 @@
 jQuery(document).ready(function ($) {
 
-   $('nav li').hover(function () {
-      $('nav li a').attr("style", "opacity: 0.4;")
-      $(this).toggleClass("active")
+   // Default 
+   // =======
 
-      $('nav li ul').removeClass("submenuactive")
-      $('nav li.active ul').addClass("submenuactive")
+   // keylist of chords
+   var sub_ul = $('.chord_list li').children()[0]
+
+   // key of chords
+   var sub_ul_li_1 = $(sub_ul).children()[0]
+   var sub_ul_li_2 = $(sub_ul).children()[1]
+   var sub_ul_li_3 = $(sub_ul).children()[2]
+   var sub_ul_li_4 = $(sub_ul).children()[3]
+   var sub_ul_li_5 = $(sub_ul).children()[4]
+   var sub_ul_li_6 = $(sub_ul).children()[5]
+   var sub_ul_li_7 = $(sub_ul).children()[6]
+
+   // key text in lyrics
+   $('.I').text($(sub_ul_li_1).text())
+   $('.II').text($(sub_ul_li_2).text())
+   $('.III').text($(sub_ul_li_3).text())
+   $('.IV').text($(sub_ul_li_4).text())
+   $('.V').text($(sub_ul_li_5).text())
+   $('.VI').text($(sub_ul_li_6).text())
+   $('.VII').text($(sub_ul_li_7).text())
+
+
+   // CLicked 
+   // =======
+   $('.chord_list li').click(function () {
+
+      // active chords
+      $('.chord_list > li').removeClass('avtive_chord')
+      $(this).addClass('avtive_chord')
+
+      // keylist of chords
+      var sub_ul = $(this).children()[0]
+
+      // key of chords
+      var sub_ul_li_1 = $(sub_ul).children()[0]
+      var sub_ul_li_2 = $(sub_ul).children()[1]
+      var sub_ul_li_3 = $(sub_ul).children()[2]
+      var sub_ul_li_4 = $(sub_ul).children()[3]
+      var sub_ul_li_5 = $(sub_ul).children()[4]
+      var sub_ul_li_6 = $(sub_ul).children()[5]
+      var sub_ul_li_7 = $(sub_ul).children()[6]
+
+      // key text in lyrics
+      $('.I').text($(sub_ul_li_1).text())
+      $('.II').text($(sub_ul_li_2).text())
+      $('.III').text($(sub_ul_li_3).text())
+      $('.IV').text($(sub_ul_li_4).text())
+      $('.V').text($(sub_ul_li_5).text())
+      $('.VI').text($(sub_ul_li_6).text())
+      $('.VII').text($(sub_ul_li_7).text())
 
    })
-   $('nav').mouseleave(function () {
-      $('nav li a').attr("style", "opacity: 1;")
-   })
 
-   $('.social-icons li').hover(function () {
-      $('.social-icons li a').attr("style", "opacity: 0.4;")
-      $(this).toggleClass("active")
-   })
-   $('.social-icons li').mouseleave(function () {
-      $('.social-icons li a').attr("style", "opacity: 1;")
-   })
-
-
-   // responsive menu
-   jQuery("a.responsive-menu").click(function () {
-      jQuery("ul.menu").fadeToggle();
-      return false;
-   });
-
-   jQuery(window).resize(function () {
-      var windowWidth = jQuery(window).width();
-      if (windowWidth > 748) {
-         jQuery("ul.menu").css({
-            "display": "block"
-         });
-         $('.responsive-menu').removeClass('change');
-      } else {
-         jQuery("ul.menu").css({
-            "display": "none"
-         });
-
-      }
-   });
 });
