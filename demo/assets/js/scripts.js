fb51ed96342ed9fb023b09d22cb31381b57ce50f
sabbirahmad00521 <sabbirahmad00521@gmail.com>
2020-07-29 08:52:01 +0600

    a

--- demo/assets/js/scripts.js
+++ demo/assets/js/scripts.js
@@ -1,71 +1,40 @@
 jQuery(document).ready(function ($) {
 
    // Owl Carousel
-
-   $('.homepage-slider').owlCarousel({
-      items: 1,
+   /*
+   $('.owl-carousel-class-name').owlCarousel({
+      items: 4,
       loop: true,
       autoplay: true,
-      autoplayTimeout: 8000,
-      dots: false,
-      nav: false,
-      smartSpeed: 1500
-   });
-
-
-   // search Scripts
-   $('.search').click(function () {
-      $('.search-box').fadeIn()
-   });
-   $('.search-box i').click(function () {
-      $('.search-box').fadeOut()
-   });
-
-   // menu
-   jQuery("a.responsive-menu").click(function () {
-      jQuery("ul.menu").fadeToggle();
-      return false;
-   });
-   $('.homepage-menu-right .language').click(function () {
-      $(this).find('ul').toggle()
-      console.log(this);
-   })
-   jQuery(window).resize(function () {
-      var windowWidth = jQuery(window).width();
-      if (windowWidth > 750) {
-         jQuery("ul.menu").css({
-            "display": "block"
-         });
-         $('.responsive-menu').removeClass('change');
-
-         jQuery("ul.menu").removeClass('mobile-menu')
-         // jQuery("ul.submenu").hide()
-      } else {
-         jQuery("ul.menu").css({
-            "display": "none"
-         });
-         jQuery("ul.menu").addClass('mobile-menu')
-         $('ul.mobile-menu > li').click(function () {
-            $(this).find('ul').toggle()
-         })
+      autoplayTimeout: 2000,
+      dots: true,
+      margin: 15,
+      nav: true,
+      navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+      smartSpeed: 1,
+      dragEndSpeed: 1,
+      fluidSpeed: 1,
+      autoplaySpeed: 10,
+      responsiveClass: true,
+      responsive: {
+         0: {
+            items: 1,
+            nav: true
+         },
+         600: {
+            items: 3,
+            nav: false
+         },
+         1000: {
+            items: 5,
+            nav: true,
+            loop: false
+         }
       }
    });
+   */
+   
+   // Other Scripts
 
-   // submenu
-   // jQuery(window).resize(function () {
-   var windowWidth = jQuery(window).width();
-   if (windowWidth < 767) {
-      jQuery("ul.menu").addClass('mobile-menu')
 
-   } else {
-      jQuery("ul.menu").removeClass('mobile-menu')
-   }
-   $('ul.mobile-menu > li').click(function () {
-      $(this).find('ul').toggle()
-   })
-   // });
-
-   // datepicker
-   $("#startdate").datepicker();
-   $("#enddate").datepicker();
 });
