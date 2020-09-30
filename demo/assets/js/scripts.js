9c92f93a36d0397e5fcc6a7faa23b7c59085d74d
sabbirahmad00521 <sabbirahmad00521@gmail.com>
2020-09-30 22:01:04 +0600

    a

--- demo/assets/js/scripts.js
+++ demo/assets/js/scripts.js
@@ -1,204 +1,79 @@
 jQuery(document).ready(function ($) {
-   // Nav script
-   $('.nav-tabs > li > a').click(function (event) {
-      event.preventDefault(); //stop browser to take action for clicked anchor
-
-      //get displaying tab content jQuery selector
-      var active_tab_selector = $('.nav-tabs > li.active > a').attr('href');
-
-      //find actived navigation and remove 'active' css
-      var actived_nav = $('.nav-tabs > li.active');
-      actived_nav.removeClass('active');
-
-      //add 'active' css into clicked navigation
-      $(this).parents('li').addClass('active');
-
-      //hide displaying tab content
-      $(active_tab_selector).removeClass('active');
-      $(active_tab_selector).addClass('hide');
-
-      //show target tab content
-      var target_tab_selector = $(this).attr('href');
-      $(target_tab_selector).removeClass('hide');
-      $(target_tab_selector).addClass('active');
-   });
+    
+    // mobile responsive
+    $('.mobile-bar').click(function() {
+        $('nav').toggleClass("sidenav")
+    })
 
 
-   $('a.get-started').click(function (event) {
-      event.preventDefault(); //stop browser to take action for clicked anchor
-
-      //get displaying tab content jQuery selector
-      var active_tab_selector = $('.nav-tabs > li.active > a').attr('href');
-
-      //find actived navigation and remove 'active' css
-      var actived_nav = $('.nav-tabs > li.active');
-      actived_nav.removeClass('active');
-
-      //add 'active' css into clicked navigation
-      $('#info-tab').parents('li').addClass('active');
-
-      //hide displaying tab content
-      $(active_tab_selector).removeClass('active');
-      $(active_tab_selector).addClass('hide');
-
-      //show target tab content
-      var target_tab_selector = $('#info-tab').attr('href');
-      $(target_tab_selector).removeClass('hide');
-      $(target_tab_selector).addClass('active');
-   });
-
-   // info script
-   $('.info-tabs > li > a').click(function (event) {
-      event.preventDefault(); //stop browser to take action for clicked anchor
-
-      //get displaying tab content jQuery selector
-      var active_info_tab_selector = $('.info-tabs > li.active > a').attr('href');
-
-      //find actived navigation and remove 'active' css
-      var actived_info_nav = $('.info-tabs > li.active');
-      actived_info_nav.removeClass('active');
-
-      //add 'active' css into clicked navigation
-      $(this).parents('li').addClass('active');
-
-      //hide displaying tab content
-      $(active_info_tab_selector).removeClass('active');
-      $(active_info_tab_selector).addClass('hide');
-
-      //show target tab content
-      var target_info_tab_selector = $(this).attr('href');
-      $(target_info_tab_selector).removeClass('hide');
-      $(target_info_tab_selector).addClass('active');
-   });
+    // autoscroll
+    // =========
+    $('.auto-scroll').click(function() {
+        if ($(this).hasClass('active')) {
+            function stop_scroll_down() {
+                clearInterval(scroll);
+            }
+            stop_scroll_down()
+            $(this).removeClass('active')
+            console.log('active class removed');
+        } else {
+            function start_scroll_down() {
+                scroll = setInterval(function() {
+                    window.scrollBy(0, 1);
+                }, 200);
+            }
+            start_scroll_down()
+            $(this).addClass('active')
+            console.log('active class added');
+        }
+    })
 
 
 
+    //  Lyrics Script
 
-   // responsive info contetn
-   var windowWidth = jQuery(window).width();
-   if (windowWidth < 991) {
-      $('.info-content h2').append('<i id="close-info" class="fa fa-times"></i>');
-
-
-      var active_info_tab_selector = $('.info-tabs > li.active > a').attr('href');
-      $(active_info_tab_selector).removeClass('active');
-      $(active_info_tab_selector).addClass('hide');
-
-      $('.info-content').removeClass('active');
-      $('.info-content').addClass('hide');
-
-      //find actived navigation and remove 'active' css
-      var actived_info_nav = $('.info-tabs > li.active');
-      actived_info_nav.removeClass('active');
-
-
-   }
-
-   jQuery(window).resize(function () {
-      var windowWidth = jQuery(window).width();
-      if (windowWidth < 991) {
-         $('.info-content.active').addClass('hide');
-         $('.info-content.active').removeClass('active');
-         $('.info-tabs li.active').addClass('hide');
-         $('.info-tabs li.active').removeClass('active');
-
-         $('.info-content h2').append('<i id="close-info" class="fa fa-times"></i>');
-
-         // var active_info_tab_selector = $('.info-tabs > li.active > a').attr('href');
-         // $(active_info_tab_selector).removeClass('active');
-         // $(active_info_tab_selector).addClass('hide');
-
-         // $('.info-content').removeClass('active');
-         // $('.info-content').addClass('hide');
-
-         // //find actived navigation and remove 'active' css
-         // var actived_info_nav = $('.info-tabs > li.active');
-         // actived_info_nav.removeClass('active');
-
-         // info script
-         $('.info-content h2 i').click(function (event) {
-
-            //get displaying tab content jQuery selector
-            var active_info_tab_selector = $('.info-tabs > li.active > a').attr('href');
-            console.log(active_info_tab_selector);
-
-            //find actived navigation and remove 'active' css
-            var actived_info_nav = $('.info-tabs > li.active');
-            actived_info_nav.removeClass('active');
-
-            //add 'active' css into clicked navigation
-            $(this).parents('li').addClass('active');
-
-            //hide displaying tab content
-            $(active_info_tab_selector).removeClass('active');
-            $(active_info_tab_selector).addClass('hide');
-
-            //show target tab content
-            var target_info_tab_selector = $(this).attr('href');
-            $(target_info_tab_selector).removeClass('hide');
-            $(target_info_tab_selector).addClass('active');
-         });
-      }
-   });
-
-   // info script
-   $('.info-content h2 i').click(function (event) {
-
-      //get displaying tab content jQuery selector
-      var active_info_tab_selector = $('.info-tabs > li.active > a').attr('href');
-      console.log(active_info_tab_selector);
-
-      //find actived navigation and remove 'active' css
-      var actived_info_nav = $('.info-tabs > li.active');
-      actived_info_nav.removeClass('active');
-
-      //add 'active' css into clicked navigation
-      $(this).parents('li').addClass('active');
-
-      //hide displaying tab content
-      $(active_info_tab_selector).removeClass('active');
-      $(active_info_tab_selector).addClass('hide');
-
-      //show target tab content
-      var target_info_tab_selector = $(this).attr('href');
-      $(target_info_tab_selector).removeClass('hide');
-      $(target_info_tab_selector).addClass('active');
-   });
-
-
-   // // info script
-   // $('.info-tabs > li > a').click(function (event) {
-   //    event.preventDefault(); //stop browser to take action for clicked anchor
-
-   //    //get displaying tab content jQuery selector
-   //    var active_info_tab_selector = $('.info-tabs > li.active > a').attr('href');
-
-   //    //find actived navigation and remove 'active' css
-   //    var actived_info_nav = $('.info-tabs > li.active');
-   //    actived_info_nav.removeClass('active');
-
-   //    //add 'active' css into clicked navigation
-   //    $(this).parents('li').addClass('active');
-
-   //    //hide displaying tab content
-   //    $(active_info_tab_selector).removeClass('active');
-   //    $(active_info_tab_selector).addClass('hide');
-
-   //    //show target tab content
-   //    var target_info_tab_selector = $(this).attr('href');
-   //    $(target_info_tab_selector).removeClass('hide');
-   //    $(target_info_tab_selector).addClass('active');
-   // });
-
-
-
-
-
-   var asass = $('#google_translate_element')
-   $('.skiptranslate ').hide()
-   // $('.goog-te-gadget').children().not("div").remove();
-   console.log(asass);
-
-
-
+    // keylist of chords
+    var sub_ul = $('.chord_list li.avtive_chord').children()[0]
+    // key of chords
+    var sub_ul_li_1 = $(sub_ul).children()[0]
+    var sub_ul_li_2 = $(sub_ul).children()[1]
+    var sub_ul_li_3 = $(sub_ul).children()[2]
+    var sub_ul_li_4 = $(sub_ul).children()[3]
+    var sub_ul_li_5 = $(sub_ul).children()[4]
+    var sub_ul_li_6 = $(sub_ul).children()[5]
+    var sub_ul_li_7 = $(sub_ul).children()[6]
+    // key text in lyrics
+    $('.I').text($(sub_ul_li_1).text())
+    $('.II').text($(sub_ul_li_2).text())
+    $('.III').text($(sub_ul_li_3).text())
+    $('.IV').text($(sub_ul_li_4).text())
+    $('.V').text($(sub_ul_li_5).text())
+    $('.VI').text($(sub_ul_li_6).text())
+    $('.VII').text($(sub_ul_li_7).text())
+    // CLicked 
+    // =======
+    $('.chord_list li').click(function() {
+        // active chords
+        $('.chord_list > li').removeClass('avtive_chord')
+        $(this).addClass('avtive_chord')
+        // keylist of chords
+        var sub_ul = $(this).children()[0]
+        // key of chords
+        var sub_ul_li_1 = $(sub_ul).children()[0]
+        var sub_ul_li_2 = $(sub_ul).children()[1]
+        var sub_ul_li_3 = $(sub_ul).children()[2]
+        var sub_ul_li_4 = $(sub_ul).children()[3]
+        var sub_ul_li_5 = $(sub_ul).children()[4]
+        var sub_ul_li_6 = $(sub_ul).children()[5]
+        var sub_ul_li_7 = $(sub_ul).children()[6]
+        // key text in lyrics
+        $('.I').text($(sub_ul_li_1).text())
+        $('.II').text($(sub_ul_li_2).text())
+        $('.III').text($(sub_ul_li_3).text())
+        $('.IV').text($(sub_ul_li_4).text())
+        $('.V').text($(sub_ul_li_5).text())
+        $('.VI').text($(sub_ul_li_6).text())
+        $('.VII').text($(sub_ul_li_7).text())
+    })
+    
 });
