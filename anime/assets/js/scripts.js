c2700eba816e1cb78d6e8891dd21f69d638fb08f
sabbirahmad00521 <sabbirahmad00521@gmail.com>
2020-12-13 23:09:21 +0600

    a
    a

--- anime/assets/js/scripts.js
+++ anime/assets/js/scripts.js
@@ -4,28 +4,37 @@ jQuery(document).ready(function($) {
     $('.manga-details-mobile > p').text($('.manga-info> p').text())
 
 
-    $(function(){
-      var sharebutton = $('.share-manga a').clone();
-      $('.social-mobile').html(sharebutton);
+    $(function() {
+        var sharebutton = $('.share-manga a').clone();
+        $('.social-mobile').html(sharebutton);
     });
-   
-   
+
+
     $('.manga-details-mobile a.manga-name').click(function() {
-       $('.manga-details-mobile > p').slideToggle();
-       $('.manga-details-mobile a.manga-name i').toggleClass('fa-angle-up', 'fa-angle-down');
- 
+        $('.manga-details-mobile > p').slideToggle();
+        $('.manga-details-mobile a.manga-name i').toggleClass('fa-angle-up', 'fa-angle-down');
+
         return false;
     })
-   
+
     $('.manga-details-mobile a.btn-share').click(function() {
-       $('.social-mobile-wrapper').slideToggle();
+        $('.social-mobile-wrapper').slideToggle();
         return false;
     })
 
 
-    //    $(window).resize(function() {
-    //       if (window.matchMedia('(max-width: 575px)').matches) {
-    //           // functionality for screens smaller than 1200px
-    //       }
-    //   });
+    $('.manga-reader').scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > 1) {
+            console.log('it worked');
+        }
+    })
+
+    // $(window).resize(function() {
+    //     if (window.matchMedia('(max-width: 575px)').matches) {
+
+    //     }
+    // });
+
+
 });
