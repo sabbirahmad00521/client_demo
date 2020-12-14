c38ae10399ddf8e4ad530f0dae018c5e3eaa89d0
sabbirahmad00521 <sabbirahmad00521@gmail.com>
2020-12-15 05:30:47 +0600

    ss

--- anime/assets/js/scripts.js
+++ anime/assets/js/scripts.js
@@ -23,36 +23,123 @@ jQuery(document).ready(function($) {
     })
 
 
-    // $('.manga-reader').scroll(function() {
-    //     var scrollTop = $(this).scrollTop();
-    //     console.log(scrollTop);
-
-    //     if (scrollTop > 1) {
-    //       var mangaa = document.querySelector('.manga-reader ')
-    //         mangaa.requestFullScreen()
-    //         console.log('hu');
-    //     }
-    // })
-
-    // $(window).resize(function() {
-    //     if (window.matchMedia('(max-width: 575px)').matches) {
-
-    //     }
-    // });
 
 
-});
 
-let last_known_scroll_position = 0;
 
-document.addEventListener('scroll', function(e) {
-    last_known_scroll_position = window.scrollY;
 
-    if (last_known_scroll_position > 1) {
-        var mangaa = document.querySelector('.manga-reader ')
-        manga.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-        // mangaa.requestFullScreen()
-        console.log('hu');
+    // if (window.matchMedia('(max-width: 575px)').matches) {
+    //     $(".chapter-browse-wrapper").appendTo(".mobile-browse");
+
+    //     $('.manga-reader').scroll(function() {
+    //         var scrollTop = $(this).scrollTop();
+    //         console.log(scrollTop);
+    //         if (($(this).scrollTop() + $(this).innerHeight() )>= ($(this)[0].scrollHeight - 200)) { 
+    //             console.log('end reached');
+    //         }
+    //         console.log($(this).innerHeight());
+    //         if (scrollTop > 100) {
+    //             var mangareader = document.querySelector('.manga-reader ')
+    //             $('.manga-reader').addClass('mobile')
+    //             $('.manga-details-mobile').addClass('mobile')
+    //         } else {
+    //             $('.manga-reader').removeClass('mobile')
+    //             $('.manga-details-mobile').removeClass('mobile')
+    //         }
+    //     })
+    // } else {
+    //     $(".chapter-browse-wrapper").appendTo(".chapter-browse");
+    // }
+
+
+
+
+
+
+    if (window.matchMedia('(max-width: 575px)').matches) {
+        $(".chapter-browse-wrapper").appendTo(".mobile-browse");
+        var fullscreenmode = false
+        $('.manga-reader').scroll(function() {
+
+            var scrollTop = $(this).scrollTop();
+            console.log('scrolltop ' + scrollTop);
+            // console.log('innerHeight ' + $(this).innerHeight());
+            // console.log('scrolltop + innerHeight ' + ($(this).scrollTop() + $(window).innerHeight()));
+            // console.log('scrollHeight ' + $(this)[0].scrollHeight);
+            // console.log('scrollHeight ' + ($(this)[0].scrollHeight -  $(window).innerHeight()) );
+            var mobileReaderscroll = $(this)[0].scrollHeight - $(window).innerHeight();
+            console.log('totalscrollsize ' + mobileReaderscroll);
+            var extrascroll = $(window).innerHeight() - $(this).innerHeight();
+            console.log('extra scroll ' + extrascroll);
+            console.log(('total and extra ' + (mobileReaderscroll + extrascroll)));
+            if (scrollTop < 100) { // যদি ১০০ এর কম স্ক্রল করা হয় তবে 
+                $('.manga-reader').removeClass('mobile')
+                $('.manga-details-mobile').removeClass('mobile')
+                $('.mobile-browse').removeClass("hide");
+                $('.manga-details-mobile').removeClass("hide");
+                fullscreenmode = false
+            } else if (scrollTop > 100 && scrollTop < mobileReaderscroll) { // যদি ১০০ এর বেশি 3743 এর কম স্ক্রল হয়
+                console.log('hey bro');
+                $('.manga-reader').addClass('mobile')
+                $('.manga-details-mobile').addClass('mobile')
+                $('.mobile-browse').addClass("hide");
+                $('.manga-details-mobile').addClass("hide");
+                fullscreenmode = true
+            } else if (scrollTop >= mobileReaderscroll) { // যদি 3743 এর বেশি স্ক্রল করা হয় তবে 
+                $('.manga-reader').removeClass('mobile')
+                $('.manga-details-mobile').removeClass('mobile')
+                $('.mobile-browse').removeClass("hide");
+                $('.manga-details-mobile').removeClass("hide");
+                fullscreenmode = false
+            }
+            
+
+        })
+        
+        $('.manga-reader img').click(function () {
+            console.log(fullscreenmode);
+            if ($('.mobile-browse').hasClass("hide") && fullscreenmode === true) {
+                console.log('nice bro');
+                $('.mobile-browse').removeClass("hide");
+            } else if(fullscreenmode === true){
+                $('.mobile-browse').addClass("hide");
+            }
+            if ($('.manga-details-mobile').hasClass("hide")  && fullscreenmode === true) {
+                $('.manga-details-mobile').removeClass("hide");
+            } else if(fullscreenmode === true){
+                $('.manga-details-mobile').addClass("hide");
+            }
+
+        })
+    } else {
+        $(".chapter-browse-wrapper").appendTo(".chapter-browse");
     }
 
+
+
 });
+
+
+
+
+// if (scrollTop < 100) { // যদি ১০০ এর কম স্ক্রল করা হয় তবে 
+//     $('.manga-reader').removeClass('mobile')
+//     $('.manga-details-mobile').removeClass('mobile')
+//     $('.mobile-browse').fadeIn();
+// } else if (scrollTop > 100 && scrollTop < mobileReaderscroll) { // যদি ১০০ এর বেশি স্ক্রল করা হয় তবে 
+//     $('.manga-reader').addClass('mobile')
+//     $('.manga-details-mobile').addClass('mobile')
+//     $('.mobile-browse').fadeOut();
+//     $('.manga-reader').click(function() {
+//         if ($('.mobile-browse').css('display') == 'none') {
+//             $('.mobile-browse').fadeIn();
+//         }
+//         if ($('.mobile-browse').css('display') == 'block') {
+//             $('.mobile-browse').fadeOut();
+//         }
+//     })
+// } else if (scrollTop >= mobileReaderscroll) { // যদি ৩০০০ এর বেশি স্ক্রল করা হয় তবে 
+//     $('.manga-reader').removeClass('mobile')
+//     $('.manga-details-mobile').removeClass('mobile')
+//     $('.mobile-browse').fadeIn();
+// }
